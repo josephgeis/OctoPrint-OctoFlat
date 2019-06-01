f1fafafe5eaba7952019dfed18b64e87abeeb782
Joseph Geis <geis28@gmail.com>
2019-06-01 15:37:41 -0700

    Implemented Theme

--- octoprint_octoflat/__init__.py
+++ octoprint_octoflat/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
+# (Don't forget to remove me)
 # This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
 # as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
 # defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
@@ -11,62 +11,52 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class OctoflatPlugin(octoprint.plugin.SettingsPlugin,
-                     octoprint.plugin.AssetPlugin,
-                     octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
+class OctoflatPlugin(octoprint.plugin.AssetPlugin):
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    # ~~ AssetPlugin mixin
 
-	##~~ AssetPlugin mixin
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            css=["css/octoflat.css"]
+        )
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/octoflat.js"],
-			css=["css/octoflat.css"],
-			less=["less/octoflat.less"]
-		)
+    # ~~ Softwareupdate hook
 
-	##~~ Softwareupdate hook
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            octoflat=dict(
+                displayName="Octoflat Plugin",
+                displayVersion=self._plugin_version,
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			octoflat=dict(
-				displayName="Octoflat Plugin",
-				displayVersion=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="juniorRubyist",
+                repo="OctoPrint-OctoFlat",
+                current=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="juniorRubyist",
-				repo="OctoPrint-OctoFlat",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/juniorRubyist/OctoPrint-OctoFlat/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/juniorRubyist/OctoPrint-OctoFlat/archive/{target_version}.zip"
+            )
+        )
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Octoflat Plugin"
+__plugin_name__ = "OctoFlat Theme"
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = OctoflatPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = OctoflatPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
