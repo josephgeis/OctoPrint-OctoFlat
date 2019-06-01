b4947791db70f261a8893f7bb98e0c4e4349f631
Joseph Geis <juniorrubyist@gmail.com>
2019-06-01 15:33:08 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# OctoPrint-OctoFlat
+
+A flat theme for OctoPrint, getting rid of the tacky gradients and adding space between the menu buttons.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/juniorRubyist/OctoPrint-OctoFlat/archive/master.zip
+
+No configuration necessary.
+
---
+++ octoprint_octoflat/static/js/octoflat.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-OctoFlat
+ *
+ * Author: Joseph Geis
+ * License: AGPLv3
+ */
+$(function() {
+    function OctoflatViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: OctoflatViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_octoflat, #tab_plugin_octoflat, ...
+        elements: [ /* ... */ ]
+    });
+});
